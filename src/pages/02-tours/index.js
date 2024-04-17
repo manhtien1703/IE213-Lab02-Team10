b007ef6803d6b8c166210ed39c8f2664fe971569
manhtien1703 <21522671@gm.uit.edu.vn>
2024-04-17 22:29:01 +0700

    Bai tap Lab02

---
+++ src/pages/02-tours/index.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./Tours.css";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+
+function ToursPage() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    fetch("https://cors-anywhere.herokuapp.com/" + url)
+      .then((response) => response.json())
+      .then((data) => {
+        setTours((preState) => [...preState, ...data]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default ToursPage;
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Trang Home</h1>
+      <div className="button-container">
+        <Link to="/birthday-reiminder">
+          <button>Birthday reminder</button>
+        </Link>
+        <Link to="/tours">
+          <button>Tours</button>
+        </Link>
+        <Link to="/reviews">
+          <button>Reviews</button>
+        </Link>
+        <Link to="/accordion">
+          <button>Accordion</button>
+        </Link>
+        <Link to="/menu">
+          <button>Menu</button>
+        </Link>
+        <Link to="/tabs">
+          <button>Tabs</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
